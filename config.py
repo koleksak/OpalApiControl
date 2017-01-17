c99007d971d6c8bd0ac99a50b4dcd216188be691
unknown <koleksak@vols.utk.edu>
2017-01-17 18:58:15 -0500

    Basic Signal control changed in real time in signal control module

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ import RtlabApi
 import os
 import glob
 import sys
+import system
+from system import acquire
+import signals
+from signals import signalcontrol
+import parameters
 from time import sleep
--- signals/signalcontrol.py
+++ signals/signalcontrol.py
@@ -70,17 +70,43 @@ def releaseSubSystem():
         print ("Subsystems:", subSystemInfo)
         subSystemName, subSystemId, nodeName = subSystemInfo
         print ("Subsystem Name: {}   Subsystem ID: {}    NodeName: {}".format(subSystemName, subSystemId, nodeName))
-    chooseReleaseId = raw_input("Choose subsystem Id to be released: ")
-    OpalApiPy.GetSignalControl(int(chooseReleaseId),0)
-    print "Subsystem %s control released." % subSystemId
+    chooseId = (int(raw_input("Choose subsystem Id to be released: ")))
+    OpalApiPy.GetSignalControl(int(chooseId),0)
+    print "Subsystem %s control released." % chooseId
 
 def accessSubSystem():
     """Access subsystem by subsystem Id (one value at a time for now)"""
+    acquire.connectToModel()
     subSystemList = OpalApiPy.GetSubsystemList()
+
+
     for subSystemInfo in subSystemList:
-        print ("Subsystems:", subSystemInfo)
+
         subSystemName, subSystemId, nodeName = subSystemInfo
         print ("Subsystem Name: {}   Subsystem ID: {}    NodeName: {}".format(subSystemName,subSystemId,nodeName))
-    chooseReleaseId = raw_input("Choose subsystem Id to be accessed: ")
-    OpalApiPy.GetSignalControl(int(chooseReleaseId), 1)
-    print "Subsystem %s control accessed." % subSystemId
+    chooseId = (int(raw_input("Choose subsystem Id to be accessed: ")))
+    OpalApiPy.GetSignalControl(chooseId, 1)
+    print "Subsystem %s control accessed." % chooseId
+
+    subSystemSignals = OpalApiPy.GetControlSignalsDescription()
+    signalType, iD, path, label, reserved, readonly, value = subSystemSignals[chooseId-1]
+    print("Subsystem Name: {}  Signal Name:  {} Subsystem ID: {}  ReadOnly: {} Value:  {}".format(subSystemName,label,chooseId,readonly,value))
+
+    # Allow chosen signal to be written to
+
+    # releaseSubSystem()
+
+
+
+
+def setSignals(signalNames,newSignalValues):
+    """Change signal values by signal subsystem names
+    signalNames can by a tuple (name1,name2....,nameN) or a single name
+    newSignalValues can also by a tuple (newValue1,newValue2,....newValueN), or a single value.
+    Item indexes in the signal tuple correspond to respective numeric values in the value tuple """
+
+
+    accessSubSystem()
+    OpalApiPy.SetSignalsByName(signalNames,newSignalValues)
+
+    print("Signal: {} Signal Value: {}".format(signalNames,newSignalValues))
