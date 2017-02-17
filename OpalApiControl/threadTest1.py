28456c811ddc58a951cc4086800ff84fc05c5271
Kellen <koleksak@vols.utk.edu>
2017-02-17 15:49:17 -0500

    Sample file for creating data list and acquisition

---
+++ OpalApiControl/threadTest1.py
@@ -0,0 +1,15 @@
+
+from config import *
+print("in File threadTest1")
+if __name__ == '__main__':
+    print("running file ThreadTest1")
+    dataStruc1 = acquisitioncontrol.DataList(1);
+    dirset = acquisitioncontrol.StartAcquisitionThread('ephasorex2','phasor01_IEEE39',dataStruc1,1,"AcqThread Set",1)
+    dirret = acquisitioncontrol.acquisitionThreadReturn('ephasorex2','phasor01_IEEE39',dataStruc1,1,"AcqThread Return",1)
+    dirret.setDaemon(True)
+    dirset.start()
+    dirret.start()
+    #Joining seems to function better if you wait for thread to start
+    sleep(3)
+    dirset.join(2)
+    dirret.join(2)
