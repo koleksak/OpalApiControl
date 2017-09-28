df92604e9659fc4222c9a36f79da844941f111a0
koleksak <kellen0leksak@gmail.com>
2017-09-28 10:18:15 -0400

    Bus Coords Extract Update

--- OpalApiControl/examples/bus_coords_extract.py
+++ OpalApiControl/examples/bus_coords_extract.py
@@ -5,6 +5,6 @@ logging.basicConfig(level=logging.INFO)
 from OpalApiControl import *
 from OpalApiControl.parser.bus_coords import BusCoords
 if __name__ == '__main__':
-    coords = BusCoords('WECCBusData.txt', 'Curent02_final_ConstZ.raw', 'WECC181', 'C:/RT-LABv11_Workspace_New/')
+    coords = BusCoords('WECCBusData.txt', 'Curent02_final_ConstZ.raw', 'WECC181', 'phasor03_PSSE', 'C:/RT-LABv11_Workspace_New/')
     coords.get_bus_coords()
     coords.append_coords_to_raw()
--- OpalApiControl/examples/wecc181_run.py
+++ OpalApiControl/examples/wecc181_run.py
@@ -6,4 +6,4 @@ logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
     run_model(project='WECC181', model='phasor03_PSSE', raw='Curent02_final_ConstZCoords.raw', dyr='Curent02_final.dyr',
-              path='C:/RT-LABv11_Workspace_New/', server='tcp://127.0.0.1:5678')
+              path='C:/RT-LABv11_Workspace_New/', server='tcp://10.129.132.192:9999')
